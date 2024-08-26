74e61c693ba8f5c9e7e41d1e3719488c898e0011
Jtoro19 <jutorop@unal.edu.co>
2024-08-26 00:01:10 -0500

    remove innecesary file

---
+++ Proyecto_web/app/Http/Controllers/ProductsCotroller.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+use App\Models\User;
+
+class ProductsCotroller extends Controller
+{
+
+    public function index()
+    {
+        $products = Product::all();
+        return view('products.index', ['products' => $products]);
+    }
+
+    public function create()
+    {
+        return view('');
+    }
+
+    public function store(Request $request)
+    {
+        $product = new Product();
+        $product->userID = $request->userID;
+        $product->productName = $request->productName;
+        $product->price = $request->price;
+        $product->stock = $request->stock;
+        $product->description = $request->description;
+        $product->able = 1;
+        $product->save();
+        return redirect()->route('products.index');
+    }
+
+    public function show($id)
+    {
+        //
+    }
+
+    public function edit($id)
+    {
+        $product = Product::find($id);
+        return view('products.edit', ['product' => $product]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $product = Product::find($id);
+        //$product->userID = $request->userID;
+        $product->productName = $request->age_restriction;
+        $product->price = $request->price;
+        $product->stock = $request->stock;
+        $product->description = $request->description;
+        $product->save();
+        return redirect()->route('products.index');
+    }
+
+    public function destroy($id)
+    {
+        $product = Product::find($id);
+        $product->able=0;
+        $product->save();
+        return redirect()->route('');
+    }
+}
