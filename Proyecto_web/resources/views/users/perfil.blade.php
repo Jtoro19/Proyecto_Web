ccbe7c170349e6135090a2d18b3d03935f7fab50
Jtoro19 <jutorop@unal.edu.co>
2024-09-15 16:47:09 -0500

    feat: shipmets_viewProfile

--- Proyecto_web/resources/views/users/perfil.blade.php
+++ Proyecto_web/resources/views/users/perfil.blade.php
@@ -36,6 +36,11 @@
                     <div class="data" id="phone">{{ $user->phoneNumber }}</div>
                 </div>
 
+                <!-- Botón "Ver envíos" arriba de direcciones -->
+                <div class="mt-4">
+                    <a class="btn btn-primary" href="{{ route('shipments.index') }}">Ver envíos</a>
+                </div>
+
                 <!-- Sección de Direcciones -->
                 <div class="address-section mt-5 d-flex justify-content-between align-items-center">
                     <div>
@@ -75,3 +80,4 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
+
